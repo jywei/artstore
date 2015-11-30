43a9ea5bf851a6590d54a8148110478ab6f67980
Roy Wei <roywjy@gmail.com>
2015-11-30 19:24:06 +0800

    Practice2_unfinished.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :admin do
     resources :products
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
   end
 
   resources :products do
@@ -13,6 +21,7 @@ Rails.application.routes.draw do
   resources :carts do
     collection do
       post :checkout
+      delete :clean
     end
   end
 
@@ -21,6 +30,12 @@ Rails.application.routes.draw do
        get :pay_with_credit_card
      end
    end
+
+  resources :items, controller: "cart_items"
+
+  namespace :account do
+    resources :orders
+  end
 
   root "products#index"
 
